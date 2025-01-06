841f49aab409eafc6838e0575cf60e31ba7c6135
Ishini <ishinishavindhya@gmail.com>
2025-01-06 19:23:57 +0530

    commit

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,16 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+const port = process.env.port || 4000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({credentials:true}));
+
+app.get('/', (req, res) => 
+    res.send("API Working fine"));
+
+app.listen(port, ()=>console.log(`listening on port : ${port}`));
+
+
