ea5c814d528f5af94c9ff1fcbaa64da496783877
Ishini <ishinishavindhya@gmail.com>
2025-01-17 18:22:50 +0530

    login

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -30,6 +30,8 @@ export const register = async (req,res)=>{
             maxAge: 7 * 24 *  60 * 60 * 1000
         });
 
+        return res.json({success: true});
+
     }catch(error){
         res.json({success:false,message: error.message});
 
@@ -47,8 +49,25 @@ export const login = async (req, res) => {
         const user = await userModel.find({email})
 
         if(!user) {
-            return res.json({success:false,message:' Invalid user '});
+            return res.json({success:false,message:' Invalid email '});
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.json({success:false,message:' Invalid password '});
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET ,{expiresIn:'7d'});
+
+        res.cookie('token',token, {
+            httpOnly:true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 *  60 * 60 * 1000
+        });
+
+        return res.json({success: true});        
 
     }catch(errr){
         res.json({success:false,message: error.message});
