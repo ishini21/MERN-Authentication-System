1965f1d08b74cef12c91dccfaab598ba47d94561
Ishini <ishinishavindhya@gmail.com>
2025-02-21 18:47:48 +0530

    commit

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -7,14 +7,29 @@ function Login() {
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
       <img src={assets.logo} alt="Logo" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
       <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
-        <h2>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
-        <p>{state === 'Sign Up' ? 'Create your account' : 'Login to your acount'}</p>
+        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
+        <p className='text-sm text-center mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your acount'}</p>
         <form>
           <div className='flex items-center gap-3 mt-4 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
           <img src={assets.person_icon} alt="" />
           <input className='bg-transparent outline-none' type="text" placeholder='Full Name' required />
           </div>
+          <div className='flex items-center gap-3 mt-4 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+          <img src={assets.mail_icon} alt="" />
+          <input className='bg-transparent outline-none' type="email" placeholder='Email id' required />
+          </div>
+          <div className='flex items-center gap-3 mt-4 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+          <img src={assets.lock_icon} alt="" />
+          <input className='bg-transparent outline-none' type="password" placeholder='Enter Password' required />
+          </div>
+
+          <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot Password?</p>
+          <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium'>{state}
+          </button>
         </form>
+        <p>Already have an account? {''}
+        <span>Login here</span>
+        </p>
       </div>
     </div>
   )
